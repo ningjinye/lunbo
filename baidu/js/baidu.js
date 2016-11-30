1431e0804254e9cf841af6196a47f646d515cf99
ningjinye <1533499090@qq.com>
2016-12-01 00:39:04 +0800

    baidu

--- baidu/js/baidu.js
+++ baidu/js/baidu.js
@@ -20,6 +20,7 @@ $(function(){
         $(".fullpage").css({marginTop:-num*heights,
             transition:"margin-top 2s ease"
         });
+        $(".btn").css({background:"none"}).eq(num).css({background:"#333"});
         flag=false;
     });
     touch.on("body","swipedown",".fullpage",function(){
@@ -34,12 +35,84 @@ $(function(){
         $(".fullpage").css({marginTop:-num*heights,
             transition:"margin-top 2s ease"
         });
+        $(".btn").css({background:"none"}).eq(num).css({background:"#333"});
+
         flag=false;
     });
 
     $(".fullpage")[0].addEventListener("webkitTransitionEnd",function(){
         flag=true;
+        $(".ship").css({
+            transform:"translate(0,0)"
+        });
+        $("section").each(function(index,obj){
+            if(index==0){
+                return;
+            }
+            if(index==num){
+                $(".aa").eq(index).css({
+                    opacity:1,
+                    transform:"translate(0,0)"
+                });
+                $(".bb").eq(index).css({
+                    opacity:1,
+                    transform:"translate(0,0)"
+                });
+            }else{
+                $(".aa").eq(index).css({
+                    opacity:0,
+                    transform:"translate(-50px,0)"
+                });
+                $(".bb").eq(index).css({
+                    opacity:0,
+                    transform:"translate(50px,0)"
+                });
+            }
+        })
+    });
+
+
+// 菜单
+    var flag2=true;
+$(".min-right").click(function(){
+    if(flag2){
+        $(".top-line").css({transform:"translate(0,4px) rotate(135deg)"});
+        $(".bottom-line").css({transform:"translate(0,-4px) rotate(-135deg)"});
+        $(".cd a").each(function(index,obj){
+            $(obj).css({opacity:0,
+                animation:"cd 1.5s linear "+index*0.2+"s forwards"
+            })
+        });
+        flag2=false;
+        $(".cd").css({display:"block"});
+    }else{
+        $(".top-line").css({transform:"translate(0,0) rotate(0deg)"});
+        $(".bottom-line").css({transform:"translate(0,0) rotate(0deg)"});
+        $(".cd a").each(function(index,obj){
+            $(obj).css({opacity:1,
+                animation:"cd1 1.5s linear "+(1.2-index)*0.2+"s forwards"
+            });
+        });
+        flag2=true;
+        setTimeout(function(){
+            $(".cd").css({display:"none"});
+        },1700);
+    }
+
+});
+    $(window).resize(function(){
+        heights=$(window).height();
+        widths=$(window).width();
+        $(".fullpage").css({marginTop:-num*heights});
+        if(widths>1000){
+            $(".top-line,.bottom-line").css({
+                transform:"translate(0,0) rotate(0deg)"
+            });
+            $(".cd a").css({
+                animation:"none"
+            });
+            flag2=true;
+        }
     })
 
-
-})
+});
